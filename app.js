62bf599fbf9437ab08e467eddca0a01ff696d144
Timo Laakkonen <timo.laakkonen@hotmail.com>
2023-04-08 14:56:44 +0300

    deploy

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000;
 
+app.use(express.static("dist"));
+
 app.get('/health', (req, res) => {
   throw 'error...'
   // eslint-disable-next-line no-unreachable
@@ -13,10 +15,6 @@ app.get('/health', (req, res) => {
 app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
 })
-
-app.use(express.static("dist"));
-
-
 
 app.listen(PORT, () => {
   console.log("server started on port 5000");
